955581a3be5b0cfbddc2f3dd19929e72024d800b
AndrewLolo <andrewloloal@gmail.com>
2016-05-18 15:04:47 +0300

    [initial] - added cylon bot | added jenkins info polling

---
+++ main.js
@@ -0,0 +1,26 @@
+const Cylon = require('cylon');
+const jenkinsapi = require('jenkins-api');
+
+const jenkins = jenkinsapi.init('http://jenkins.marks.kyiv.epam.com/');
+
+
+
+jenkins.last_build_info('01.0_POS_DEV1_buildShared', function(err, data) {
+    if (err){ return console.log(err); }
+    console.log(data)
+});
+
+
+Cylon.robot({
+    connections: {
+        raspi: { adaptor: 'raspi' }
+    },
+
+    devices: {
+        led: { driver: 'led', pin: 11 }
+    },
+
+    work: function(my) {
+        every((1).second(), my.led.toggle);
+    }
+}).start();
