490c21c33d31d8b48479310abf4995fe3df73f06
Viktor Novak <valakar@gmail.com>
2016-05-18 16:16:07 +0300

    [main.js] - add switcher for build status

--- main.js
+++ main.js
@@ -1,24 +1,47 @@
+'use strict';
+
 const Cylon = require('cylon');
 const jenkinsapi = require('jenkins-api');
 
 const jenkins = jenkinsapi.init('http://jenkins.marks.kyiv.epam.com/');
 
-jenkins.last_build_info('01.0_POS_DEV1_buildShared', function(err, data) {
+jenkins.last_build_info('01.0_POS_DEV1_buildShared', onSuccess);
+
+function onSuccess(err, data) {
     if (err){ return console.log(err); }
-    console.log(data)
-});
+    console.log(data);
 
-
-Cylon.robot({
-    connections: {
-        raspi: { adaptor: 'raspi' }
-    },
-
-    devices: {
-        led: { driver: 'led', pin: 11 }
-    },
-
-    work: function(my) {
-        every((1).second(), my.led.toggle);
+    let buildStatus = data.result;
+    let buildingStatus = data.building;
+    let led = {
+        driver: 'led'
     }
-}).start();
+    switch (buildStatus) {
+        case 'FAILED':
+            led.pin = 16;
+            break;
+
+        case 'SUCCESS':
+            led.pin = 11;
+            break;
+
+        default:
+            break;
+    }
+
+    Cylon.robot({
+        connections: {
+            raspi: { adaptor: 'raspi' }
+        },
+
+        devices: {led},
+
+        work: function(my) {
+            if (buildingStatus) {
+                every((1).second(), my.led.toggle);
+            } else {
+                my.led.turnOn();
+            }
+        }
+    }).start();
+}
